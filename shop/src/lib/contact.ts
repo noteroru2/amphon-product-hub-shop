4d959775f7504aef685a40bc6356dac243f367e2
noteroru2 <noteroru2@gmail.com>
2026-09-14 22:19:37 +0700

    feat(shop): add official brand contact links

--- shop/src/lib/contact.ts
+++ shop/src/lib/contact.ts
@@ -1,2 +1,4 @@
 export const primaryLineId = '@webuy'
 export const primaryLineUrl = 'https://line.me/R/ti/p/@webuy'
+export const facebookPageUrl = 'https://facebook.com/amphontrading'
+export const mainWebsiteUrl = 'https://amphon.co.th/'
