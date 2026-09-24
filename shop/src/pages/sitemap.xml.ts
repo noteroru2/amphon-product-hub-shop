777460c46fa0ebcddcf0325eeee55ad00fbb1562
noteroru2 <noteroru2@gmail.com>
2026-09-24 20:58:34 +0700

    feat(shop): register spec and image sitemaps

--- shop/src/pages/sitemap.xml.ts
+++ shop/src/pages/sitemap.xml.ts
@@ -7,7 +7,9 @@ export const GET: APIRoute = () => {
     absoluteUrl('/sitemap-static.xml'),
     absoluteUrl('/sitemap-categories.xml'),
     absoluteUrl('/sitemap-evergreen.xml'),
+    absoluteUrl('/sitemap-specs.xml'),
     absoluteUrl('/sitemap-products.xml'),
+    absoluteUrl('/sitemap-images.xml'),
   ]
   const body = `<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${sitemaps.map((url) => `  <sitemap><loc>${xmlEscape(url)}</loc></sitemap>`).join('\n')}\n</sitemapindex>\n`
   return xmlResponse(body)
